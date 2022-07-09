bfc255958c4b496e9655e970b1dcb6375b8162f2
joaolucastbr@gmail.com <joaolucastbr@gmail.com>
2022-07-09 16:59:20 -0300

    Adicionar arquivos de projeto.

---
+++ exame 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exame_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Números de 1 a 50");
+            int i = 0;
+            for (i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
